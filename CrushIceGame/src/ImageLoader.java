55bc9a020d18013069f87db182247cfcf0f31d8b
rkrn.zerozerozerooox@gmail.com <44431860+fukuchiyuki1225@users.noreply.github.com>
2019-12-30 01:13:56 +0900

    ImageLoader使えたので一旦コミットプッシュ

--- CrushIceGame/src/ImageLoader.java
+++ CrushIceGame/src/ImageLoader.java
@@ -1,4 +1,20 @@
+import java.awt.image.BufferedImage;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
 
 public class ImageLoader {
+	static ImageLoader il = new ImageLoader();
 
+	public static BufferedImage readImage(String path) {
+		BufferedImage bi = null;
+		try {
+			URL url = il.getClass().getClassLoader().getResource(path);
+			System.out.println(url);
+			bi = ImageIO.read(url);
+		} catch (Exception e) {
+			e.getStackTrace();
+		}
+		return bi;
+	}
 }
