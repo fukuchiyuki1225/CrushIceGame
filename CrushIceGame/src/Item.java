72f5fa17aa18a7724fed33345f1271ad2bdbd056
rkrn.zerozerozerooox@gmail.com <44431860+fukuchiyuki1225@users.noreply.github.com>
2020-01-13 01:06:03 +0900

    アイテムクラスとアイテム画像の追加

--- CrushIceGame/src/Item.java
+++ CrushIceGame/src/Item.java
@@ -1,4 +1,18 @@
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 
 public abstract class Item {
+	public JLabel itemlabel;
+	public String name;
 
+	public Item(String imgPass, String name) {
+		this.itemlabel = new JLabel(new ImageIcon(ImageLoader.loadImage(imgPass)));
+		this.name = name;
+	}
+
+	public abstract void use();
+
+	public String getName() {
+		return name;
+	}
 }
