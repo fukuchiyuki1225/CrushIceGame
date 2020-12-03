7300f5b49672f588ec82c76ef66aae5c23f9ba6e
yuki-f <rkrn.zerozerozerooox@gmail.com>
2020-12-03 14:29:22 +0900

    Ghostアイテムクラスのファイルを作成

---
+++ CrushIceGame/src/Ghost.java
@@ -0,0 +1,12 @@
+
+public class Ghost extends Item {
+
+	public Ghost(String imgPass, String name, int location) {
+		super(imgPass, name, location);
+	}
+
+	@Override
+	public void use() {
+	}
+
+}
